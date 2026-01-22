b51f4140a1fab16e00576ef0500c193bd0f551c7
StenSipma <sten.sipma@ziggo.nl>
2026-01-22 14:03:19 +0100

    Add a first version of a cron_job script

---
+++ scripts/cron_job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+DATA_DIR=$HOME/cronjob-data
+BLAAUW_DIR=$HOME/blaauw-archive
+
+NOW=$(shell date +%Y%m%d-%H%M%S)
+
+YESTERDAY=$(date -d "`date`-2days" +%F) # YYYY-MM-DD
+
+GBT_FILE=$DATA_DIR/${YESTERDAY}-raw-headers.GBT.pickle
+
+echo "Crawling..."
+python3 $BLAAUW_DIR/scripts/crawler.py --output $DATA_DIR --date $YESTERDAY --base GBT
+
+echo "Inserting"
+python3 $BLAAUW_DIR/scripts/insert.py --file $GBT_FILE
+
+echo "Removing"
+rm $GBT_FILE
+
+# python3 $BLAAUW_DIR/scripts/crawler.py --output data/ --from-date 250101 --to-date 260101 --base GBT
+# LDST
