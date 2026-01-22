3d8038a9b9853f61e9e8b3fe00d40bd1c0e3e51d
StenSipma <sten.sipma@ziggo.nl>
2026-01-22 14:15:40 +0100

    Fix LDST cron

--- scripts/cron-job-ldst.sh
+++ scripts/cron-job-ldst.sh
@@ -9,7 +9,7 @@ BLAAUW_DIR=$HOME/blaauw-archive
 LAST_MONTH=$(date -d "`date`-30days" +%F) # YYYY-MM-DD
 TODAY=$(date +%F) # YYYY-MM-DD
 
-LDST_FILE=$DATA_DIR/${YESTERDAY}-raw-headers.LDST.pickle
+LDST_FILE=$DATA_DIR/${LAST_MONTH}-${TODAY}-raw-headers.LDST.pickle
 
 echo "Crawling..."
 python3 $BLAAUW_DIR/scripts/crawler.py --output $DATA_DIR --from-date $LAST_MONTH --to-date $TODAY --base LDST
